a8b9b04024478c92439d17b7c69e84941ca6be47
bear (Mike Taylor) <bear@andyet.net>
2015-10-28 09:33:16 -0700

    add aaronpk's site to test alternative link header formats

--- tests/test_indieauth.py
+++ tests/test_indieauth.py
@@ -2,23 +2,21 @@
 
 import unittest
 from ninka import discoverAuthEndpoints, validateAuthCode
-from urlparse import ParseResult
 
-
-_url = "https://bear.im/"
 
 class TestDiscovery(unittest.TestCase):
     def runTest(self):
-        r = discoverAuthEndpoints(_url)
+        for url in ('https://bear.im', 'http://aaronparecki.com/'):
+            r = discoverAuthEndpoints(url)
 
-        assert 'authorization_endpoint' in r
+            assert 'authorization_endpoint' in r
 
-        authURL = None
-        for url in r['authorization_endpoint']:
-            authURL = url
-            break
+            authURL = None
+            for endpoint_url in r['authorization_endpoint']:
+                authURL = endpoint_url
+                break
 
-        assert authURL is not None
-        assert authURL.scheme == 'https'
-        assert authURL.netloc == 'indieauth.com'
-        assert authURL.path   == '/auth'
+            assert authURL is not None
+            assert authURL.scheme == 'https'
+            assert authURL.netloc == 'indieauth.com'
+            assert authURL.path   == '/auth'
